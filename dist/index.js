c6fc839a1b2a2bc4dcc1ac053da4f44781b179da
govind07 <govindrajput02653@gmail.com>
2024-10-26 15:40:06 +0530

    tsc

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let Name = "govind";
+const age = 21;
+let product = 5;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 let Name:string = "govind";
 const  age:number =  21 ;
+
+let  product : number | string | boolean = 5;
+
+type Person = {
+    name :string,
+    id:number,
+    email:string,
+    password:number,
+
+    
+}
+
